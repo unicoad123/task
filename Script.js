5cde9b6e832cdba230e047f1bec8b9c33dfd49a3
unicoad123 <41484199+unicoad123@users.noreply.github.com>
2022-02-25 12:32:21 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -10,6 +10,27 @@ for(var i=0;i<items.length;i++)
 {
     items[i].style.backgroundColor='#ccc';
 }
+------Query Selector---------------------------------
+var item=document.querySelector('#list:nthchild(2)');
+item.style.backgroundColor='green';
+
+var item=document.querySelectorAll('#list:nthchild(3)');
+item.style.visibility='hidden';
+
+----------------Query SelectorAll------------------------
+var item=document.querySelector('#list');
+items[2].style.color='green';
+
+var odd=document.querySelector('li:nth-child(odd)');
+for(var i=0;i<odd.length;i++)
+{
+    odd[i].style.backgroundColor='green';
+}
+
+diff b/w queryselector and queryselectorall:
+querySelector() returns a single object with the first HTML element that matches the selectors
+querySelectorAll() returns an array of objects with all the HTML elements that match the selectors
+
 
 -----------------------GetElementByClassName---------------------------
 var li=document.getElementByClassName('list');
