d37added9fda7905d6d541df530db4409fcc2e29
unicoad123 <41484199+unicoad123@users.noreply.github.com>
2022-02-25 11:57:37 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -10,3 +10,16 @@ for(var i=0;i<items.length;i++)
 {
     items[i].style.backgroundColor='#ccc';
 }
+
+-----------------------GetElementByClassName---------------------------
+var li=document.getElementByClassName('list');
+console.log(items);
+console.log(items[1]);
+items[1].textContent='Hello';
+items[1].style.fontWeight='bold';
+items[1].style.backgroundColor='green';
+
+for(var i=0;i<item.length;i++)
+{
+    items[i].style.backgroundColor='#f4f4f4';
+}
