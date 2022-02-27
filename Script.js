48e2323c2f66493e0ba497b8eb5e540b131b09b4
unicoad123 <41484199+unicoad123@users.noreply.github.com>
2022-02-27 10:18:44 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -107,7 +107,22 @@ function removeItem(e){
     }
   }
 }
+----------------filter------------------
+   function filterItems(e){
+ 
+      var text = e.target.value.toLowerCase();
 
+      var items = itemList.getElementsByTagName('li');
+     
+      Array.from(items).forEach(function(item){
+        var itemName = item.firstChild.textContent;
+        if(itemName.toLowerCase().indexOf(text) != -1){
+          item.style.display = 'block';
+        } else {
+          item.style.display = 'none';
+        }
+      });
+    }
 
 
 
