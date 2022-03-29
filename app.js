237bbe8b775f3a9738059ddf51e728b92b881407
unicoad123 <41484199+unicoad123@users.noreply.github.com>
2022-03-29 11:38:49 +0530

    Routers and filters

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const app = express();
+
+const adminRoutes=require('./routes/admin')
+const shopRoutes=require('./routes/shop')
 
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/add-product', (req, res, next) => {
-res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
-app.use('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
+app.use('/admin',adminRoutes)
+app.use(shopRoutes)
 
-app.use('/', (req, res, next) => {
-res.send('<h1>Hello from Express!</h1>');
-});
+app.use((req,res,next)=>
+{
+   res.status(404).send('<h1>Page not Found</h1>')
+})
   
   app.listen(3000);
