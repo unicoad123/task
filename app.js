ecce49f3dec681cdcf78ae4c3dcff14f37d3a0a9
unicoad123 <41484199+unicoad123@users.noreply.github.com>
2022-03-29 10:54:08 +0530

    Middlewares

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-const http = require('http');
-const route=require('./routes')
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
-const server = http.createServer(route);
+app.use(bodyParser.urlencoded({extended:false}));
 
-server.listen(2000);
+app.use('/add-product', (req, res, next) => {
+res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+res.send('<h1>Hello from Express!</h1>');
+});
+  
+  app.listen(3000);
