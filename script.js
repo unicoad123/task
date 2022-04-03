6cbb8ffe5761fdacf4c40a10abf4900a937a39ca
unicoad123 <41484199+unicoad123@users.noreply.github.com>
2022-04-03 20:55:37 +0530

    Hamburger-Button

---
+++ script.js
@@ -0,0 +1,8 @@
+btn=document.getElementById('btn');
+nav=document.getElementById('nav');
+
+btn.addEventListener("click",()=>
+{
+    nav.classList.toggle('active');
+    btn.classList.toggle('active');
+})
