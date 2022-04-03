06324f3f39bfccf28c37c059ddf81739ed988ff2
unicoad123 <41484199+unicoad123@users.noreply.github.com>
2022-04-03 21:47:01 +0530

    Toast Notification

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-btn=document.getElementById('btn');
-nav=document.getElementById('nav');
-
+const btn=document.getElementById("btn");
+const container=document.getElementById("container")
 btn.addEventListener("click",()=>
 {
-    nav.classList.toggle('active');
-    btn.classList.toggle('active');
+    createNotification();
 })
+function createNotification()
+{
+    const notif=document.createElement("div");
+    notif.classList.add("toast");
+
+    notif.innerText="This is Toast Notification";
+    container.appendChild(notif);
+
+    setTimeout(()=>
+    {
+        notif.remove();
+    },3000)
+
+}
