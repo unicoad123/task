7f396be2a7fab1062ca4c20744a328f8ba2b4f0f
unicoad123 <41484199+unicoad123@users.noreply.github.com>
2022-04-04 14:51:13 +0530

    Script For adding to cart

--- script.js
+++ script.js
@@ -1,19 +1,43 @@
-const container=document.getElementById("container");
-const img=document.querySelector("img");
+const cart_items = document.querySelector('#cart .cart-items');
 
-container.addEventListener("mousemove",(e)=>
+
+const Container = document.getElementById('EcommerceContainer');
+
+
+Container.addEventListener('click',(e)=>
 {
-    const x=e.clientX-e.target.offsetLeft;
-    const y=e.clientY-e.target.offsetTop;
-
-    console.log(x,y);
-
-    img.style.transformOrigin=`${x}px ${y}px`;
-    img.style.transform="scale(2)";
-});
-
-container.addEventListener("mouseleave",()=>
-{
-    img.style.transformOrigin="center center";
-    img.style.transform="scale(1)";
+    if (e.target.className=='shop-item-button'){
+        const id = e.target.parentNode.parentNode.id
+        const name = document.querySelector(`#${id} h3`).innerText;
+        const img_src = document.querySelector(`#${id} img`).src;
+        const price = e.target.parentNode.firstElementChild.firstElementChild.innerText;
+        let total_cart_price = document.querySelector('#total-value').innerText;
+        if (document.querySelector(`#in-cart-${id}`))
+        {
+            alert('This item is already added to the cart');
+            return
+        }
+        document.querySelector('.cart-number').innerText = parseInt(document.querySelector('.cart-number').innerText)+1
+        const cart_item = document.createElement('div');
+        cart_item.classList.add('cart-row');
+        cart_item.setAttribute('id',`in-cart-${id}`);
+        total_cart_price = parseFloat(total_cart_price) + parseFloat(price)
+        total_cart_price = total_cart_price.toFixed(2)
+        document.querySelector('#total-value').innerText = `${total_cart_price}`;
+        cart_item.innerHTML = `
+        <span class='cart-item cart-column'>
+        <img class='cart-img' src="${img_src}" alt="">
+            <span>${name}</span>
+    </span>`
+    const container = document.getElementById('container');
+        const notification = document.createElement('div');
+        notification.classList.add('notification');
+        notification.innerHTML = `<h4>Your Product : <span>${name}</span> is added to the cart<h4>`;
+        container.appendChild(notification);
+        setTimeout(()=>{
+            notification.remove();
+        },2500)
+}
+    
 })
+
